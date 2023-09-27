677c64409d292273f680adb16b10bdbdae21d747
Timothy Huo <61216890+timothyhuo1@users.noreply.github.com>
2023-09-27 11:57:04 -1000

    flutter signin signup template

---
+++ tadish/android/app/src/main/kotlin/com/example/tadish/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tadish
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tadish/lib/app.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'pages/signin/signin_view.dart';
+import 'pages/signup/signup_view.dart';
+import 'settings/settings_controller.dart';
+import 'settings/settings_view.dart';
+
+/// The Widget that configures your application.
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+    required this.settingsController,
+  });
+
+  final SettingsController settingsController;
+
+  @override
+  Widget build(BuildContext context) {
+    // Glue the SettingsController to the MaterialApp.
+    //
+    // The ListenableBuilder Widget listens to the SettingsController for changes.
+    // Whenever the user updates their settings, the MaterialApp is rebuilt.
+    return ListenableBuilder(
+      listenable: settingsController,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          theme: ThemeData(),
+          darkTheme: ThemeData.dark(),
+          themeMode: settingsController.themeMode,
+
+          // Define a function to handle named routes in order to support
+          // Flutter web url navigation and deep linking.
+          onGenerateRoute: (RouteSettings routeSettings) {
+            return MaterialPageRoute<void>(
+              settings: routeSettings,
+              builder: (BuildContext context) {
+                switch (routeSettings.name) {
+                  case SettingsView.routeName:
+                    return SettingsView(controller: settingsController);
+                  case SigninView.routeName:
+                    return const SigninView();
+                  case SignupView.routeName:
+                    return const SignupView();
+                  default:
+                    return const SigninView();
+                }
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
