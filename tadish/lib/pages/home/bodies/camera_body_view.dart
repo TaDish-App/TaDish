3d2d4010c6ab35f0be912846faaf28d544550f02
Alyssia Chen <84742988+alyssia-chen@users.noreply.github.com>
2023-10-01 12:05:15 -1000

    Form and headers

--- tadish/lib/pages/home/bodies/camera_body_view.dart
+++ tadish/lib/pages/home/bodies/camera_body_view.dart
@@ -1,18 +1,75 @@
 import 'package:flutter/material.dart';
 
 /// Displays a list of Gardens.
-class CameraBodyView extends StatelessWidget {
-  const CameraBodyView({
-    super.key,
-  });
+/// CameraBodyView
+class CameraBodyView extends StatefulWidget {
+  const CameraBodyView({super.key});
+
+  @override
+  CameraBodyViewState createState() {
+    return CameraBodyViewState();
+  }
+}
+
+class CameraBodyViewState extends State<CameraBodyView> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Text('Camera Page'),
-      ),
+    // Build a Form widget using the _formKey created above.
+    return Container(
+        padding: const EdgeInsets.all(32),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              // Dish Name
+              const Text(
+                'Kimchi Fried Rice',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  height: 2,
+                  fontSize: 30,
+                ),
+              ),
+              const Text(
+                'HiTea Cafe',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+              // Add TextFormFields and ElevatedButton here.
+              TextFormField(
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false otherwise.
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, display a snackbar. In the real world,
+                    // you'd often call a server or save the information in a database.
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')),
+                    );
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        )
     );
   }
 }
