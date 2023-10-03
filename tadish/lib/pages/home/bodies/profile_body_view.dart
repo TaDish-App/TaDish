3f6e30448333f45e1d0fc8f14ce954c57d26cc2f
Alyssia Chen <84742988+alyssia-chen@users.noreply.github.com>
2023-10-03 10:51:08 -1000

    Radar chart, name, username, location

--- tadish/lib/pages/home/bodies/profile_body_view.dart
+++ tadish/lib/pages/home/bodies/profile_body_view.dart
@@ -1,21 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:radar_chart/radar_chart.dart';
 
-import '../../../components/garden_summary_card.dart';
-
-/// Displays a list of Gardens.
 class ProfileBodyView extends StatelessWidget {
   const ProfileBodyView({
     super.key,
   });
 
-  final String title = 'Gardens';
-
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Text('Profile Page'),
+    return Center(
+      child: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            // Radar Chart for taste prefs
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: const ShapeDecoration(
+                  color: Colors.white,
+                  shape: CircleBorder(
+                      side: BorderSide(
+                    width: 2,
+                    color: Colors.brown,
+                  ))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  RadarChart(
+                    length: 4,
+                    radius: 40,
+                    // initialAngle: pi / 5,
+                    backgroundColor: Colors.white,
+                    borderStroke: 2,
+                    borderColor: Colors.grey.shade200,
+                    radialStroke: 2,
+                    radialColor: Colors.grey.shade200,
+                    radars: [
+                      RadarTile(
+                        values: const [.95, .54, .2, .87],
+                        borderStroke: 2,
+                        borderColor: Colors.yellow,
+                        backgroundColor: Colors.yellow.withOpacity(0.4),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              "Timothy Huo",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.person),
+                Text("thuo_hawaii  | "),
+                Icon(Icons.pin_drop_rounded),
+                Text(" Honolulu, HI"),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
