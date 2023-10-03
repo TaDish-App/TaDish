16e621b1ffe7e5694b12307db081c9e41e04cad7
Timothy Huo <61216890+timothyhuo1@users.noreply.github.com>
2023-10-02 16:01:43 -1000

    Update surprise_body_view.dart

--- tadish/lib/pages/home/bodies/surprise_body_view.dart
+++ tadish/lib/pages/home/bodies/surprise_body_view.dart
@@ -21,7 +21,7 @@ class _SurpriseBodyViewState extends State<SurpriseBodyView> {
   @override
   Widget build(BuildContext context) {
     final items = <String>[
-      '1', 
+      '1',
       '2',
       '3',
       '4',
@@ -35,52 +35,53 @@ class _SurpriseBodyViewState extends State<SurpriseBodyView> {
     int colorIndex = 0;
 
     return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          setState(() {
-            final selectedValue = Fortune.randomInt(0, items.length);
-            selected.add(selectedValue);
-            result = selectedValue;
-          });
-        },
-        child: Column(
-          children: [
-            const SizedBox(height: 60),
-            const Text(
-              'Spin the Wheel',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Expanded(
-              child: FortuneWheel(
-                selected: selected.stream,
-                indicators: const <FortuneIndicator>[
-                  FortuneIndicator(
-                    alignment: Alignment.topCenter,
-                    child: TriangleIndicator(
-                      color: customPrimaryColor,
-                    ),
-                  ),
-                ],
-                items: [
-                  for (var i = 0; i < items.length; i++)
-                    FortuneItem(
-                      child: Text(items[i]),
-                      style: FortuneItemStyle(
-                        color: alternatingColors[i % alternatingColors.length],
+      body: Column(
+        children: [
+          const SizedBox(height: 60),
+          const Text(
+            'Spin the Wheel',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    final selectedValue = Fortune.randomInt(0, items.length);
+                    selected.add(selectedValue);
+                    result = selectedValue;
+                  });
+                },
+                child: FortuneWheel(
+                  selected: selected.stream,
+                  indicators: const <FortuneIndicator>[
+                    FortuneIndicator(
+                      alignment: Alignment.topCenter,
+                      child: TriangleIndicator(
+                        color: customPrimaryColor,
                       ),
                     ),
-                ],
-              ),
+                  ],
+                  items: [
+                    for (var i = 0; i < items.length; i++)
+                      FortuneItem(
+                        child: Text(items[i]),
+                        style: FortuneItemStyle(
+                          color: alternatingColors[i %
+                              alternatingColors.length],
+                        ),
+                      ),
+                  ],
+                ),
+              )
+          ),
+          const SizedBox(height: 20), // Adjust spacing as needed
+          if (result >= 0) // Display result if a valid result is available
+            Text(
+              'Selected Result: ${items[result]}',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 20), // Adjust spacing as needed
-            if (result >= 0) // Display result if a valid result is available
-              Text(
-                'Selected Result: ${items[result]}',
-                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            const SizedBox(height: 60),
-          ],
-        ),
+          const SizedBox(height: 60),
+        ],
       ),
     );
   }
