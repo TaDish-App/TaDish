83356e33724bb53e717c93bba56fd77374a9bedf
Timothy Huo <61216890+timothyhuo1@users.noreply.github.com>
2023-10-03 08:34:33 -1000

    recommendation page update

--- tadish/lib/pages/home/bodies/recommendation_body_view.dart
+++ tadish/lib/pages/home/bodies/recommendation_body_view.dart
@@ -4,14 +4,15 @@ class RecommendationBodyView extends StatelessWidget {
   const RecommendationBodyView({
     super.key,
   });
+  // final image = const AssetImage('assets/images/garden-001.jpg');
 
-  final image = const AssetImage('assets/images/recommend/1.jpg');
+  final image = const AssetImage('assets/images/1.jpg');
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(10.0),
         child: Card(
           child: Column(
             mainAxisSize: MainAxisSize.min,
