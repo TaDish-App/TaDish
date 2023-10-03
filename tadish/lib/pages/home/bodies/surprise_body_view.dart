2e0ef142a308ac6e9948b730abaea275594257ea
Timothy Huo <61216890+timothyhuo1@users.noreply.github.com>
2023-10-02 17:13:27 -1000

    spin the wheel rework

--- tadish/lib/pages/home/bodies/surprise_body_view.dart
+++ tadish/lib/pages/home/bodies/surprise_body_view.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import '../../../components/roll_button.dart';
+
 import 'dart:async';
+import 'dart:math';
 import '../../../custom_theme.dart';
 
-class SurpriseBodyView extends StatefulWidget {
-  @override
-  _SurpriseBodyViewState createState() => _SurpriseBodyViewState();
-  }
-
-class _SurpriseBodyViewState extends State<SurpriseBodyView> {
-  StreamController<int> selected = StreamController<int>();
-  int result = -1;
-
-  @override
-  void dispose() {
-    selected.close();
-    super.dispose();
-  }
-
+class SurpriseBodyView extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final items = <String>[
@@ -27,61 +17,60 @@ class _SurpriseBodyViewState extends State<SurpriseBodyView> {
       '4',
       '5',
       '6',
-      'TacoBell',
+      '7',
       '8',
     ];
 
     final alternatingColors = [customLightBrownColor, customDarkBrownColor];
-    int colorIndex = 0;
 
-    return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 60),
-          const Text(
-            'Spin the Wheel',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    final selectedValue = Fortune.randomInt(0, items.length);
-                    selected.add(selectedValue);
-                    result = selectedValue;
-                  });
-                },
-                child: FortuneWheel(
-                  selected: selected.stream,
-                  indicators: const <FortuneIndicator>[
-                    FortuneIndicator(
-                      alignment: Alignment.topCenter,
-                      child: TriangleIndicator(
-                        color: customPrimaryColor,
-                      ),
-                    ),
-                  ],
-                  items: [
-                    for (var i = 0; i < items.length; i++)
-                      FortuneItem(
+    StreamController<int> selected = useStreamController<int>();
+
+    final selectedIndex = useStream(selected.stream, initialData: 0).data ?? 0;
+    final isAnimating = useState(false);
+
+    void handleRoll() {
+      selected.add(
+        roll(items.length),
+      );
+    }
+
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 8),
+            const Text(
+              'Spin the Wheel',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            RollButtonWithPreview(
+              selected: selectedIndex,
+              items: items,
+              onPressed: isAnimating.value ? null : handleRoll,
+            ),
+            const SizedBox(height: 8),
+            Expanded(
+              child: FortuneWheel(
+                selected: selected.stream, // Use the same stream here
+                onAnimationStart: () => isAnimating.value = true,
+                onAnimationEnd: () => isAnimating.value = false,
+                onFling: handleRoll,
+                items: [
+                  for (var i = 0; i < items.length; i++)
+                    FortuneItem(
                         child: Text(items[i]),
+                        onTap: () => print(items[i]),
                         style: FortuneItemStyle(
                           color: alternatingColors[i %
                               alternatingColors.length],
                         ),
                       ),
-                  ],
-                ),
-              )
-          ),
-          const SizedBox(height: 20), // Adjust spacing as needed
-          if (result >= 0) // Display result if a valid result is available
-            Text(
-              'Selected Result: ${items[result]}',
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ],
+              ),
             ),
-          const SizedBox(height: 60),
-        ],
+          ],
+        ),
       ),
     );
   }
