c9c377cbf6644a7b1a0e0f49bf5e3bbf4cd3d8d6
Alyssia Chen <84742988+alyssia-chen@users.noreply.github.com>
2023-11-10 00:59:56 -1000

    Check initial seed data on app start

--- tadish/lib/main.dart
+++ tadish/lib/main.dart
@@ -2,14 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'features/ratings/domain/rating.dart';
 import 'firebase_options.dart';
 import 'app.dart';
 
 
+// Check that Freezed data models and json data files are compatible.
+Future<bool> verifyInitialData() async {
+  await Rating.checkInitialData();
+
+  return true;
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await verifyInitialData();
   runApp(const ProviderScope(child: MyApp()));
 }
 
