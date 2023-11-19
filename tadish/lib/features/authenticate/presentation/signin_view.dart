1ebc5aa70484be8ffa2d484f2481b15f13609bd7
Timothy Huo <61216890+timothyhuo1@users.noreply.github.com>
2023-11-19 11:29:37 -1000

    checkpoint auth

--- tadish/lib/features/authenticate/presentation/signin_view.dart
+++ tadish/lib/features/authenticate/presentation/signin_view.dart
@@ -1,78 +1,54 @@
-import 'package:firebase_ui_auth/firebase_ui_auth.dart' hide ForgotPasswordView;
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../home/presentation/home_view.dart';
 import 'decorations.dart';
-import 'forgot_password_view.dart';
-import 'verify_email_view.dart';
 
-/// Builds the page containing fields to enter a username and password, plus buttons.
 class SignInView extends StatelessWidget {
-  const SignInView({Key? key}) : super(key: key);
+ const SignInView({super.key});
+ 
+ static const routeName = '/signin';
 
-  static const routeName = '/signin';
-
-  @override
-  Widget build(BuildContext context) {
-    return SignInScreen(
-      actions: [
-        ForgotPasswordAction((context, email) {
-          Navigator.pushNamed(
-            context,
-            ForgotPasswordView.routeName,
-            arguments: {'email': email},
-          );
-        }),
-        AuthStateChangeAction<SignedIn>((context, state) {
-          if (!state.user!.emailVerified) {
-            Navigator.pushNamed(context, VerifyEmailView.routeName);
-          } else {
-            Navigator.pushReplacementNamed(context, HomeView.routeName);
-          }
-        }),
-        AuthStateChangeAction<UserCreated>((context, state) {
-          if (!state.credential.user!.emailVerified) {
-            Navigator.pushNamed(context, VerifyEmailView.routeName);
-          } else {
-            Navigator.pushReplacementNamed(context, HomeView.routeName);
-          }
-        }),
-        AuthStateChangeAction<CredentialLinked>((context, state) {
-          if (!state.user.emailVerified) {
-            Navigator.pushNamed(context, VerifyEmailView.routeName);
-          } else {
-            Navigator.pushReplacementNamed(context, HomeView.routeName);
-          }
-        }),
-      ],
-      styles: const {
-        EmailFormStyle(signInButtonVariant: ButtonVariant.filled),
-      },
-      headerBuilder: headerImage('assets/images/cloche_dark.png'),
-      sideBuilder: sideImage('assets/images/cloche_dark.png'),
-      subtitleBuilder: (context, action) {
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 8),
-          child: Text(
-            action == AuthAction.signIn
-                ? 'Welcome to TaDish! Please sign in.'
-                : 'Welcome to TaDish! Please create an account.',
-          ),
-        );
-      },
-      footerBuilder: (context, action) {
-        return Center(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 16),
-            child: Text(
-              action == AuthAction.signIn
-                  ? 'By signing in, you agree to our terms and conditions.'
-                  : 'By registering, you agree to our terms and conditions.',
-              style: const TextStyle(color: Colors.grey),
-            ),
-          ),
-        );
-      },
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+   return StreamBuilder<User?>(
+     stream: FirebaseAuth.instance.authStateChanges(),
+     builder: (context, snapshot) {
+       if (!snapshot.hasData) {
+         return SignInScreen(
+           providers: [
+             EmailAuthProvider(),
+           ],
+           headerBuilder: headerImage('assets/images/cloche_dark.png'),
+           sideBuilder: sideImage('assets/images/cloche_dark.png'),
+           subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  action == AuthAction.signIn
+                      ? 'Welcome to TaDish! Please sign in.'
+                      : 'Welcome to TaDish! Please create an account.',
+                ),
+              );
+              },
+           footerBuilder: (context, action) {
+              return Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text(
+                    action == AuthAction.signIn
+                        ? 'By signing in, you agree to our terms and conditions.'
+                        : 'By registering, you agree to our terms and conditions.',
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                ),
+              );
+            },
+         );
+       }
+       return HomeView();
+     },
+   );
+ }
 }
